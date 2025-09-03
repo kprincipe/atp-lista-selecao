c8aee48144362d174d783b0db2cca2b83de784c3
kprincipe <kinanprincipe@outlook.com>
2025-09-03 09:29:02 -0400

    Correcao de bug de comparacao e formatacao

--- 11_menor_numero.c
+++ 11_menor_numero.c
@@ -12,10 +12,14 @@ int main(void) {
     scanf("%d", &n2);
 
     // processamento e saida de dados inseridos
-    if (n1 > n2) {
-        printf("Maior numero: %d\n", n1);
+    printf("\n");
+    printf("----------------\n");
+    if (n1 < n2) {
+        printf("Menor numero: %d\n", n1);
+        printf("----------------\n");
         return 0;
     }
-    printf("Maior numero: %d\n", n2);
+    printf("Menor numero: %d\n", n2);
+    printf("----------------\n");
     return 0;
 }
